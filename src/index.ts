4fba432d1319b54ff2c4fad860cd558e5f081750
gnuhc <jonnnc@gmail.com>
2022-07-27 21:54:48 -0700

    Add basic implementation of slash commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const foo = true;
+export * from "./builtins";
+export * from "./decorators";
+export * from "./errors";
+export * from "./installer";
+export * from "./metadata";
+export * from "./models";
+export * from "./parsers";
+export * from "./services/SlashCommandBotService";
+export * from "./services/SlashCommandFactory";
+export * from "./services/SlashCommandService";
